29e02f339a41b5a50750d4d917becaf0ac3bf450
Olesya Vetoshkina <our_post-office@mail.ru>
2023-07-22 01:37:31 +0300

    second commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import * as crypto from "crypto";
+import fs from "fs";
 
 const app = express();
 
@@ -22,7 +23,283 @@ let posts = [
   {
     id: crypto.randomUUID(),
     name: "Олеся",
-    content: "Привет!",
+    content: "1!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "2!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "3!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "4!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "5!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "6!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "7!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "8!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "9!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "10!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "11!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "12!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "13!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "14!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "15!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "16!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "17!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "18!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "19!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "20!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "21!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "22!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "23!",
+    created: Date.now(),
+    status: false,
+    coordinates: "[55.9153, 38.1092]",
+    img: null,
+    audio: null,
+    video: null,
+    fileName: null,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Олеся",
+    content: "24!",
     created: Date.now(),
     status: false,
     coordinates: "[55.9153, 38.1092]",
@@ -42,6 +319,7 @@ let users = [
 
 let currentUser;
 let pinnedPost = { content: 'Мыши плакали, кололись, но продолжали грызть кактус (с)' };
+let searchPosts;
 
 app.use(async (request, response) => {
   const { method, id } = request.query;
@@ -59,10 +337,28 @@ app.use(async (request, response) => {
       response.send(JSON.stringify(pinnedPost)).end();
       break;
     }
+    case "searchByWord": {
+      const createData = request.body.word.toLowerCase();
+      
+      searchPosts = posts.filter((post) => post.content.toLowerCase().includes(createData));
+      if (!searchPosts) {
+        response
+          .status(404)
+          .send(JSON.stringify({ message: "Post not found" }))
+          .end();
+        break;
+      }
+      response.send(JSON.stringify(searchPosts)).end();
+      break;
+    }
     case "getPinnedPost": {      
       response.send(JSON.stringify(pinnedPost)).end();
       break;
-    }       
+    }   
+    case "getSearch": {      
+      response.send(JSON.stringify(searchPosts)).end();
+      break;
+    }    
     case "userByName": {
       const createData = request.body;     
       const user = users.find((client) => client.user === createData.user);      
@@ -92,13 +388,22 @@ app.use(async (request, response) => {
           video: createData.video,
           fileName: createData.fileName,
         };
+        const timestamp = Date.now();
+        const filename = `upload_${timestamp}`;
+        
+        const file = fs.createWriteStream(`./uploads/${filename}`);
+        request.pipe(file);
+        
+        request.on('end', () => {
+          response.send('Файл успешно загружен!');
+        });  
         posts.push(newPost);
         response.send(JSON.stringify(newPost)).end();
       } catch (error) {
         response.status(500).send(JSON.stringify({ error: error.message }));
       }
       break;
-    }
+    }    
     case "allPostsByUser": {
       let currentPosts = posts.filter((post) => post.name === currentUser);
       if (currentPosts.length === 0) {
@@ -176,7 +481,7 @@ app.use(async (request, response) => {
       response.status(404).end();
       break;
   }
-  console.log(posts);
+  // console.log(posts);
 });
 
 const port = process.env.PORT || 3000;
